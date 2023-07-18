df5ff9dbf96b9fbfca79feb27f35e5440d1f724f
Kreidli Ádám <79991580+Kredam@users.noreply.github.com>
2023-07-18 20:44:16 +0000

    chore: created account model scheme

--- account/models.py
+++ account/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Account(models.Model):
+   
+    class Types(models.TextChoices):
+        CASH = 'CASH', 'Cash'
+        CREDIT_CARD = 'CCARD', 'Credit card'
+        DEBIT_CARD = 'DCARD', 'Debit card'
+
+    expire = models.DateField(null=True)
+    type = models.CharField(choices=Types.choices, null=False)
+    number = models.BigIntegerField(null=True, max_length=19)
+    balance = models.BigIntegerField(null=False, blank=False, default=0)
+    description = models.CharField(null=False, blank=True)
+    holder = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
