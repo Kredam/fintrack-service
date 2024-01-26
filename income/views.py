75cf9a74adf44b7a256cea789d06de58df21c92e
Kreidli Ádám <79991580+Kredam@users.noreply.github.com>
2024-01-26 16:47:14 +0100

    feat: filtering and default values for listing

--- income/views.py
+++ income/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet
+from .models import Income
+from .serializers import IncomeSerializer
+from django.utils import timezone
 
 # Create your views here.
+class IncomeViewSet(ModelViewSet):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+
+    def get_queryset(self):
+        current_date = timezone.now().date()
+        default_params = {
+            'type': None,
+            'account': None,
+            'period_start': None,
+            'period_end': current_date,
+        }
+
+        # dictionary comprehension
+        params = { key: self.request.GET.get(key, default) for key, default in default_params.items() }
+
+        if params['period_start']:
+            return self.queryset.filter(period_start__gte=params['period_start'], period_end__lte=params['period_end'])
+
+        if params['account']:
+            return self.queryset.filter(account=params['account'])
+        
+        if params['type']:
+            return self.queryset.filter(type=params['type'])
+
+        return self.queryset
+    
+    def delete_by_period(self):
+        # delete record in given period
+        pass
+
+    def delete_by_type(self):
+        # delete record with certain type in given period
+        pass
